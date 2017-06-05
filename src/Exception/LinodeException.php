7f4e17fecf1894c4bc70ff65989f2dc4d92f888c
Michael Phillips <michaeljoelphillips@gmail.com>
2017-06-04 23:03:11 -0500

    Commit missing exceptions.

---
+++ src/Exception/LinodeException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LinodeApi\Exception;
+
+/**
+ * Class LinodeException
+ * @author Michael Phillips <michaeljoelphillips@gmail.com>
+ */
+class LinodeException extends \Exception
+{
+}
---
+++ src/Exception/ModelNotInitializedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LinodeApi\Exception;
+
+use LinodeApi\Exception\LinodeException;
+
+/**
+ * Class ModelNotInitializedException
+ * @author Michael Phillips <michaeljoelphillips@gmail.com>
+ */
+class ModelNotInitializedException extends LinodeException
+{
+}
